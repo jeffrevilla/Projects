f2895fd7d6375147ce47377fba030c33d0d0eb56
jeffrevilla <159680386+jeffrevilla@users.noreply.github.com>
2024-09-06 13:59:57 +0800

    Update game.js

--- Quiz Game/game.js
+++ Quiz Game/game.js
@@ -1,45 +1,84 @@
 const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName('choice-text'));
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+console.log(choices);
 
 let currentQuestion = {};
-let accecptingAnswers = true;
+let accecptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
 let questions = [
     {
-        question: "Inside which HTML element do we put the Javascript??",
-        choice1: "<script>",
-        choice2: "<Javascript>",
-        choice3: "<js>",
-        choice4: "<scripting>",
-        answer: 1
+        question: "Which company developed the Python programming language?",
+        choice1: "Microsoft",
+        choice2: "Google",
+        choice3: "Sun Microsystems",
+        choice4: "Python Software Foundation",
+        answer: 4
     },
+    {
+        question: "Which of the following is not a JavaScript data type?",
+        choice1: "String",
+        choice2: "Boolean",
+        choice3: "Alert",
+        choice4: "Number",
+        answer: 3
+    },
+    {
+        question: "Which CSS property controls the text size?",
+        choice1: "font-style",
+        choice2: "font-size",
+        choice3: "text-size",
+        choice4: "text-style",
+        answer: 2
+    }
+
 ]
 
-//Constants
+//CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS =3;
+const MAX_QUESTIONS = 3;
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
-    availableQuestions = [...questions];
+    availableQuestions = [...questions]
     console.log(availableQuestions);
     getNewQuestion();
 };
 
-getNewQuestion = () =>{
+getNewQuestion = () => {
+    
+    if (availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS){
+        return window.location.assign("/end.html")
+    }
+
     questionCounter++;
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
+    const quetionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[quetionIndex];
     question.innerText = currentQuestion.question;
 
-    choices.forEach( choice =>{
-        const number = choice.dataset ['number'];
-        choice.innerText = currentQuestion['choice' + number]; 
-    })
+    choices.forEach(choice => {
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion["choice"+ number];
+    });
+
+    availableQuestions.splice(quetionIndex, 1);
+
+    accecptingAnswers = true;
 };
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if (!accecptingAnswers) return;
+
+        accecptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+        console.log(selectedAnswer);
+        getNewQuestion();
+    });
+});
 
 startGame();
